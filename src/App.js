3ea50fe5c80650bb4766c6919f48fc614950f388
Shawn McPeek <shawnmcpeek@gmail.com>
2024-04-27 16:15:30 -0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/App.css
    	modified:   src/App.js
    	new file:   src/Faelorehn-xBo5.ttf
    	new file:   src/JapanRichRegular-vm7G9.ttf
    	new file:   src/Nintypo-rgwZx.otf
    	new file:   src/WakandaFanther-JRpp7.ttf
    	new file:   src/fr-fal1.ttf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import logo from "./swords.png";
 import "./App.css";
@@ -6,15 +6,35 @@ import Pills from "./components/nav_bar/nav_bar.components";
 import TextContent from "./components/textContent/textContent";
 import WebDevComponent from "./components/web_dev.component/web_dev.component";
 import PythonDevComponent from "./components/python_dev.component/python_dev.component";
+import ContactComponent from "./components/contact.component/contact.component";
 
 /**
  * Renders the main application component, including routing and component composition.
  *
  * @return {JSX.Element} The main application component
  */
-import ContactComponent from "./components/contact.component/contact.component";
-
 function App() {
+  const [fonts] = useState([
+    "Arial, sans-serif",
+    "Wakanda",
+    "JapanRich",
+    "Faelorehn",
+    "Nintypo",
+    "Futurama",
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+      const titleElement = document.querySelector(".App-title");
+      if (titleElement) {
+        titleElement.style.fontFamily = randomFont;
+      }
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [fonts]);
+
   return (
     <Router>
       <div className="gradient-background">
